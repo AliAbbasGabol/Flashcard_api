d425222a2908b63dec3451090f3e8c03f077a957
Ali Abbas <120602595+AliAbbasGabol@users.noreply.github.com>
2026-01-30 12:50:04 +0500

    connecting database

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
 from flask import Flask, request
+from supabase import create_client
+
+supabase = create_client("https://yencscdknhajbandqzyy.supabase.co", "sb_publishable_QSJ-iEp8QEb1XHS6jkKtZA_rcDn_CUx")
 
 app = Flask(__name__)
 
@@ -6,7 +9,7 @@ app = Flask(__name__)
 @app.after_request
 def add_cors_headers(response):
     response.headers["Access-Control-Allow-Origin"] = "https://aliabbasgabol.github.io"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS, PUT, DELETE"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
     return response
 
@@ -19,13 +22,32 @@ def index():
 
 @app.route("/Flashcards", methods= ["GET"])
 def Outputer():
-    return output
+    response = supabase.table('FLash_Cards').select("*").execute()
+    return response.data
 
 
 @app.route("/Flashcards", methods= ["POST"])
 def trnaslator():
-    Flashcard = {"question":request.json["question"], "answer":request.json["answer"]}
-    output.append(Flashcard)
-    print(output)
-    return Flashcard
+    data = request.json
+    response = supabase.table('FLash_Cards').insert(data).execute()
+    return {
+    "message": "Requested Flash card created sucessfully ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧, can be found on site✌️",
+    "data": response.data
+        }
 
+@app.route("/Flashcards", methods= ["PUT"])
+def updator():
+    data = request.json
+    response = supabase.table("FLash_Cards").update({"question": data['question'], "answer": data['answer']}).eq("id", data["id"]).execute()
+    return {
+        "message":"Requested data Updated sucessfully 🔄🔔🛠️",
+        "data": response.data
+        }
+
+@app.route("/Flashcards", methods= ["DELETE"])
+def deletor():
+    data = request.json
+    response = supabase.table("FLash_Cards").delete().eq("id", data['id']).execute()
+    return { "meeesage": "🗑️ Requested data deleted sucessfully ( -_•)▄︻テحكـ━一💥�️",
+    "data":response.data
+    }
